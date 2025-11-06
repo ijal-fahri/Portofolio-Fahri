eacc709911b6456064bb37e27c10d68899a55eeb
ijal-fahri <aprizalf753@gmail.com>
2025-11-06 11:09:29 +0700

    coba

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,9 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: "#3b82f6",
-        secondary: "#64748b ",
-        dark: "#0f172a",
+        primary: '#4F46E5',
+        secondary: '#6B7280', 
+        accent: '#3B82F6', 
       },
       screens: {
         "2xl": "1320px",
